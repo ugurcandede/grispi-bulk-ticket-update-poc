368c322c0f52cfec0f63e259b4a412f9691805e6
Uğurcan Dede <7129112+ugurcandede@users.noreply.github.com>
2023-05-21 23:30:41 +0300

    rename text files (#3)
    * rename text files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,6 @@ public class Main {
 
     public static void main(String[] args) {
         // clear console
-        System.out.print("\033[H\033[2J");
         System.out.flush();
 
         Logger.info("Application started\n");
